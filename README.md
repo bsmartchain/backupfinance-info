ae750b4b497d55e4119ca0c4595c91eca2a9410d
unknown <heavengwen35@gmail.com>
2021-04-21 02:53:03 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# EasyBake Info
+
+Analytics site for the [EasyBake Protocol](https://easybake.finance).
+
+### To Start Development
+
+###### Installing dependencies
+```bash
+yarn
+```
+
+###### Running locally
+```bash
+yarn start
+```
---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/EasyBakeSwap/easybakeswap-info" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://easybake.finance" rel="noopener noreferrer" target="_blank">
+      EasyBake
+    </a>{' '}
+    |{' '}
+    <a href="https://i.gifer.com/yy3.gif" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution
